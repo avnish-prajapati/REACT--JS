cf38219577e1341cb9e498775b7ef2e9b36dca5b
avnish-prajapati <avnishprajapati664@gmail.com>
2026-06-25 17:09:51 +0530

    Update project

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import Photos from "./screens/Photos";
 
 export default function App() {
-  return (
-    <>
-      <Photos />
-    </>
-  );
+  return <>{/* <Photos /> */}</>;
 }
